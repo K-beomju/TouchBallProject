72902c3d0dc29032056e7c78b9f111f32aebefb5
Kimbeomju <kbj52264291@gmail.com>
2023-02-06 22:39:31 +0900

    작업 내용 푸쉬

--- TouchBallProject/Assets/01.Scripts/Press.cs
+++ TouchBallProject/Assets/01.Scripts/Press.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Press : MonoBehaviour
 {
     public void ChangePressTransform()
     {
-        transform.position = new Vector3(-transform.position.x, Random.Range(-4,5),0);
+        float targetX = -transform.position.x;
+        transform.position = new Vector3(targetX + (transform.position.x > 0 ? -0.5f : 0.5f), Random.Range(-4, 5), 0);
+        transform.DOMoveX(targetX, 0.1f);
     }
 }
