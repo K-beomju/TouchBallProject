6f01e508ec7f2639955dd6d70f8ae33e1ed443ac
Kimbeomju <kbj52264291@gmail.com>
2023-02-10 22:28:38 +0900

    UI 뚫리는 버그 고침

--- TouchBallProject/Assets/01.Scripts/Object/Ball.cs
+++ TouchBallProject/Assets/01.Scripts/Object/Ball.cs
@@ -35,7 +35,7 @@ public class Ball : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            if (EventSystem.current.IsPointerOverGameObject() == false)
+            if (!IsPointerOverUIObject())
             {
                 if (isStart)
                 {
@@ -113,6 +113,16 @@ public class Ball : MonoBehaviour
                 ItemManager.Instance.SpawnItem();
             }
         }
+    }
+
+    private bool IsPointerOverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+ 
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
     }
 
 
