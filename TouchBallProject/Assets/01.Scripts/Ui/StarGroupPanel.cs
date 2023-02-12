0771b1036a6ce163d8bba5b9873641a346d492b6
Kimbeomju <kbj52264291@gmail.com>
2023-02-12 21:49:09 +0900

    스킨 패널 작업

--- TouchBallProject/Assets/01.Scripts/Ui/StarGroupPanel.cs
+++ TouchBallProject/Assets/01.Scripts/Ui/StarGroupPanel.cs
@@ -6,8 +6,14 @@ using DG.Tweening;
 
 public class StarGroupPanel : MonoBehaviour
 {
+    private RectTransform rtm;
     [SerializeField] private RectTransform starRtm;
     [SerializeField] private Text starText;
+
+    private void Awake() 
+    {
+        rtm = GetComponent<RectTransform>();
+    }
 
     public void ShowStar(int star)
     {
@@ -18,5 +24,13 @@ public class StarGroupPanel : MonoBehaviour
     {
         starRtm.DORotate(new Vector3(0, 0, 360), 0.5f, RotateMode.FastBeyond360);
         DataManager.Instance.AddStar();
+    }
+
+    public void ChangeSkinPos(bool change)
+    {
+        if(change)
+        rtm.anchoredPosition = new Vector2(0, -200);
+        else
+        rtm.anchoredPosition = Vector2.zero;
     }
 }
